c065bff2365bfeef1e1f1d174d7f51305bb0fb7c
EmailOct <39873297+EmailOct@users.noreply.github.com>
2018-08-09 14:21:01 -0500

    Update WrapperWindow.py

--- WrapperWindow.py
+++ WrapperWindow.py
@@ -1,63 +1,87 @@
-import tkinter as tk
 from tkinter import *
 import GraphCreator as gc
 
-names = ()
-entry = [0 for i in range(4)]
+class MainClass(Tk):
 
-column_values = [0,1,2,3,4]
+    def __init__(self):
+        Tk.__init__(self)
 
+        self.container = Frame(self)
+        self.container.pack(side="top", fill="both")
 
-def get_names():
-    return names
+        self.frames = {}
+        tkvar = 0
 
+        frame1 = MainPage(parent=self.container, controller=self)
+        frame1.grid(row=0, column=0, sticky="nsew")
+        frame1.tkraise()
 
-def get_entry():
-    return entry
+    def make_NamePage(self, num):
+        frame2 = NamePage(parent=self.container, controller=self, num=num)
+        frame2.grid(row=0, column=0, sticky="nsew")
+        frame2.tkraise()
 
-
-# Button Commands
-
-def analyze_data():
-    global names
-    names = (get_entry()[0].get(), get_entry()[1].get(), get_entry()[2].get(), get_entry()[3].get())
-    create_graph_select_window()
-
-
-# Chart Select Window
-
-def create_graph_select_window():
-    chart_select_window = Tk()
-    Button(chart_select_window, text="Bar Graph", command=lambda: gc.create_bar_graph(get_names(), "", column_values)).grid(row=0, column=0, pady=10)
-    Button(chart_select_window, text="Pie Chart", command=lambda: gc.create_pie_chart(get_names())).grid(row=0, column=1, pady=10)
-    Button(chart_select_window, text="Line Graph", command=lambda: gc.create_line_graph(get_names())).grid(row=1, column=0, pady=10)
-    Button(chart_select_window, text="Chart").grid(row=1, column=1, pady=10)
-    chart_select_window.mainloop()
+    def make_GraphPage(self, entries):
+        names = []
+        for entry in entries:
+            names.append(entry.get())
+        print(names)
+        frame3 = GraphPage(parent=self.container, controller=self, names=names)
+        frame3.grid(row=0, column=0, sticky="nsew")
+        frame3.tkraise()
 
 
-# Root Window
+class MainPage(Frame):
 
-def create_root_window():
-    global entry
-    root = Tk()
-    Label(root, text="Name 1").grid(row=0, pady=4)
-    entry[0] = Entry(root)
-    entry[0].grid(row=0, column=1)
-    Label(root, text="Name 2").grid(row=1, pady=4)
-    entry[1] = Entry(root)
-    entry[1].grid(row=1, column=1)
-    Label(root, text="Name 3").grid(row=2, pady=4)
-    entry[2] = Entry(root)
-    entry[2].grid(row=2, column=1)
-    Label(root, text="Name 4").grid(row=3, pady=4)
-    entry[3] = Entry(root)
-    entry[3].grid(row=3, column=1)
-    Button(root, text="Submit", command=analyze_data).grid(row=4, pady=4)
-    root.mainloop()
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        self.controller = controller
 
+        num_frame = Frame(self)
+        num_frame.grid(row=0, column=0, padx=100, pady=100)
 
-def main():
-    create_root_window()
+        Label(num_frame, text='No. of Arguments:').grid(row=0, column=0)
+
+        tkvar = StringVar(self)
+        nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        lis = OptionMenu(num_frame, tkvar, *nums)
+        lis.grid(row=1, column=0)
+
+        btn_frame = Frame(self)
+        btn_frame.grid(row=1, column=0)
+        Button(btn_frame, text='Submit', command=lambda: controller.make_NamePage(int(tkvar.get()))).pack(pady='5')
 
 
-main()
+class NamePage(Frame):
+
+    def __init__(self, parent, controller, num):
+        Frame.__init__(self, parent)
+        self.controller = controller
+
+        entries = []
+        names = []
+
+        for i in range(num):
+            Label(self, text="Name:").grid(row=i, column=0)
+            entry = Entry(self)
+            entries.append(entry)
+            entry.grid(row=i, column=1, pady=5)
+
+        Button(self, text="Submit", command=lambda: controller.make_GraphPage(entries)).grid(row=i+1, pady=4)
+
+
+class GraphPage(Frame):
+
+    def __init__(self, parent, controller, names):
+        Frame.__init__(self, parent)
+        self.controller = controller
+
+        Button(self, text="Bar Graph").grid(row=0, column=0, pady=10)
+        Button(self, text="Pie Chart", command=lambda: gc.create_pie_chart(names)).grid(row=0, column=1, pady=10)
+        Button(self, text="Line Graph", command=lambda: gc.create_line_graph(names)).grid(row=1, column=0, pady=10)
+        Button(self, text="Area Graph", command=lambda:gc.create_area_graph(names)).grid(row=1, column=1, pady=10)
+
+
+if __name__ == "__main__":
+        app = MainClass()
+        app.mainloop()
